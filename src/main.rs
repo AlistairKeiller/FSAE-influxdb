5417e5819b1d08eb0340e502741ffd2ea2dbc377
Alistair Keiller <alistair@keiller.net>
2024-10-14 05:17:12 +0000

    test

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use std::time::Duration;
+use socketcan::{CanSocket, Socket};
+use embedded_can::Frame as EmbeddedFrame;
+
+fn main() {
+    // change to can0 once we have hardware
+    let mut sock = loop {
+        match CanSocket::open("vcan0") {
+            Ok(socket) => break socket,
+            Err(_) => {
+                println!("Failed to open socket, retrying...");
+            }
+        }
+    };
+
+    loop {
+        if let Ok(frame) = sock.read_frame_timeout(Duration::from_millis(100)) {
+            println!("{:?}", frame.data());
+        }
+    }
+}
