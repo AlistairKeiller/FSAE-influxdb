54831e43b1c21c18846cd8467d95801c4fd66981
Alistair Keiller <alistair@keiller.net>
2024-10-22 00:38:19 +0000

    test

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,37 @@
-use std::time::Duration;
-use socketcan::{CanSocket, Socket};
+use chrono::{DateTime, Utc};
 use embedded_can::Frame as EmbeddedFrame;
 use influxdb::{Client, Error, InfluxDbWriteable};
-use chrono::{DateTime, Utc};
-
-
+use socketcan::{CanSocket, Id, Socket, StandardId};
+use std::time::Duration;
 
 #[derive(InfluxDbWriteable)]
 struct PackReading {
     time: DateTime<Utc>,
     voltage: i16,
     current: i16,
-    highest_temp: i8,
-    avg_temp: i8,
-    relay: i8,
 }
 
-fn crc(data: &[u8]) -> u8 {
-    let mut crc = 0u8;
+// fn crc(data: &[u8]) -> u8 {
+//     let mut crc = 0u8;
 
-    for &byte in data {
-        for i in 0..8 {
-            if ((crc >> 7) ^ (byte >> i) & 0x01) != 0 {
-                crc = (crc << 1) ^ 0x07;
-            } else {
-                crc <<= 1;
-            }
-        }
-    }
+//     for &byte in data {
+//         for i in 0..8 {
+//             if ((crc >> 7) ^ (byte >> i) & 0x01) != 0 {
+//                 crc = (crc << 1) ^ 0x07;
+//             } else {
+//                 crc <<= 1;
+//             }
+//         }
+//     }
 
-    crc
-}
+//     crc
+// }
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     // change to can0 once we have hardware
     let sock = loop {
-        match CanSocket::open("vcan0") {
+        match CanSocket::open("can0") {
             Ok(socket) => break socket,
             Err(_) => {
                 println!("Failed to open socket, retrying...");
@@ -50,27 +45,25 @@ async fn main() -> Result<(), Error> {
     loop {
         if let Ok(frame) = sock.read_frame_timeout(Duration::from_millis(100)) {
             let data = frame.data();
-            if data.len() >= 8 {
-                let pack_reading = PackReading {
-                    time: Utc::now(),
-                    voltage: u16::from_be_bytes([data[0], data[1]]),
-                    current: u16::from_be_bytes([data[2], data[3]]),
-                    highest_temp: data[4],
-                    avg_temp: data[5],
-                    relay: data[6],
-                };
+            if let Some(id) = StandardId::new(0x03B) {
+                if data.len() >= 5 && frame.id() == Id::Standard(id) {
+                    let pack_reading = PackReading {
+                        time: Utc::now(),
+                        voltage: i16::from_be_bytes([data[0], data[1]]),
+                        current: i16::from_be_bytes([data[2], data[3]])
+                    };
 
-                if crc(&data[..7]) != data[7] {
-                    eprintln!("CRC mismatch, skipping frame");
-                    continue;
-                }
-            
+                    // if crc(&data[..7]) != data[7] {
+                    //     eprintln!("CRC mismatch, skipping frame");
+                    //     continue;
+                    // }
 
-                if let Err(e) = client.query(pack_reading.into_query("pack")).await {
-                    eprintln!("Failed to write to InfluxDB: {}", e);
+                    if let Err(e) = client.query(pack_reading.into_query("pack")).await {
+                        eprintln!("Failed to write to InfluxDB: {}", e);
+                    }
+                } else {
+                    eprintln!("Received frame with insufficient data length");
                 }
-            } else {
-                eprintln!("Received frame with insufficient data length");
             }
         }
     }
