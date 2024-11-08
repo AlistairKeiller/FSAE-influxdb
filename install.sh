c241afb08685698f42fb80ace6f58b5fd96fd203
Alistair Keiller <alistair@keiller.net>
2024-11-07 20:43:18 -0800

    update

--- install.sh
+++ install.sh
@@ -5,5 +5,5 @@ sudo cp data.service /etc/systemd/system/data.service
 
 # for USB gadget
 sudo touch /boot/firmware/ssh
-# setup with /boot/firmware/config.txt dtoverlay=dwc2
+# setup with /boot/firmware/config.txt dtoverlay=dwc2 and # otg_mode=1
 # setup with /boot/firmware/cmdline.txt modules-load=dwc2,g_ether
