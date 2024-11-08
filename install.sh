7ea3b0753a7d026c72a747680279e091c50c2fb8
Alistair Keiller <alistair@keiller.net>
2024-11-08 10:55:19 +0000

    enable systemd-networkd

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 source install_influx.sh
 sudo cp 80-can.network /etc/systemd/network/80-can.network
 sudo cp data.service /etc/systemd/system/data.service
+sudo systemctl enable systemd-networkd
 
 # for USB gadget
 # sudo touch /boot/firmware/ssh
