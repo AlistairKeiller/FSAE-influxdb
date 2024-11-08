c043e514db0ca2073d887222af6bebfd79f24ce0
Alistair Keiller <alistair@keiller.net>
2024-11-08 01:22:00 -0800

    comment out

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ sudo cp 80-can.network /etc/systemd/network/80-can.network
 sudo cp data.service /etc/systemd/system/data.service
 
 # for USB gadget
-sudo touch /boot/firmware/ssh
+# sudo touch /boot/firmware/ssh
 # setup with /boot/firmware/config.txt dtoverlay=dwc2 and # otg_mode=1
 # setup with /boot/firmware/cmdline.txt modules-load=dwc2,g_ether
