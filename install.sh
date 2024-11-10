9218c5dff3e8fffcdfbc5dd662fd222a36349f92
Alistair Keiller <alistair@keiller.net>
2024-11-10 01:21:59 +0000

    use dockerfile

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-source install_influx.sh
+wget -q https://repos.influxdata.com/influxdata-archive_compat.key
+echo '393e8779c89ac8d958f81f942f9ad7fb82a25e133faddaf92e15b16e6ac9ce4c influxdata-archive_compat.key' | sha256sum -c && cat influxdata-archive_compat.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/influxdata-archive_compat.gpg > /dev/null
+echo 'deb [signed-by=/etc/apt/trusted.gpg.d/influxdata-archive_compat.gpg] https://repos.influxdata.com/debian stable main' | sudo tee /etc/apt/sources.list.d/influxdata.list
+rm influxdata-archive_compat.key
+
+sudo apt-get update && sudo apt-get install -y influxdb
+sudo service influxdb start
+influx -execute "CREATE DATABASE data"
+
 sudo cp 80-can.network /etc/systemd/network/80-can.network
 sudo cp data.service /etc/systemd/system/data.service
 sudo systemctl enable systemd-networkd
