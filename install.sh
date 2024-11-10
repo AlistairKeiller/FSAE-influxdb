6d4cc697b541dff9f7e5d5c0d5608ae5a94633e6
Alistair Keiller <alistair@keiller.net>
2024-11-09 18:24:25 -0800

    test

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ influx -execute "CREATE DATABASE data"
 sudo mkdir -p /etc/apt/keyrings/
 wget -q -O - https://apt.grafana.com/gpg.key | gpg --dearmor | sudo tee /etc/apt/keyrings/grafana.gpg > /dev/null
 echo "deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main" | sudo tee /etc/apt/sources.list.d/grafana.list
-sudo apt-get update && apt-get install -y grafana
+sudo apt-get update && sudo apt-get install -y grafana
 sudo /bin/systemctl enable grafana-server
 sudo /bin/systemctl start grafana-server
 
