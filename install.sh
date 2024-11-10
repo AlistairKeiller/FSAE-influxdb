a82bf5f5b02da9a4d3b705b11189da7f9be74cfc
Alistair Keiller <alistair@keiller.net>
2024-11-09 18:10:06 -0800

    add graphana

--- install.sh
+++ install.sh
@@ -3,10 +3,15 @@ wget -q https://repos.influxdata.com/influxdata-archive_compat.key
 echo '393e8779c89ac8d958f81f942f9ad7fb82a25e133faddaf92e15b16e6ac9ce4c influxdata-archive_compat.key' | sha256sum -c && cat influxdata-archive_compat.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/influxdata-archive_compat.gpg > /dev/null
 echo 'deb [signed-by=/etc/apt/trusted.gpg.d/influxdata-archive_compat.gpg] https://repos.influxdata.com/debian stable main' | sudo tee /etc/apt/sources.list.d/influxdata.list
 rm influxdata-archive_compat.key
-
 sudo apt-get update && sudo apt-get install -y influxdb
 sudo service influxdb start
 influx -execute "CREATE DATABASE data"
+
+sudo mkdir -p /etc/apt/keyrings/
+wget -q -O - https://apt.grafana.com/gpg.key | gpg --dearmor | sudo tee /etc/apt/keyrings/grafana.gpg > /dev/null
+echo "deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main" | sudo tee /etc/apt/sources.list.d/grafana.list
+sudo apt-get update && apt-get install -y grafana
+sudo service grafana-server start
 
 sudo cp 80-can.network /etc/systemd/network/80-can.network
 sudo cp data.service /etc/systemd/system/data.service
