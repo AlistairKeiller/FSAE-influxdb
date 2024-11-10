44290e0a51d493797f7fcb4c599878330801875d
Alistair Keiller <alistair@keiller.net>
2024-11-10 01:13:31 +0000

    remove usb gadget

--- install.sh
+++ install.sh
@@ -3,8 +3,3 @@ source install_influx.sh
 sudo cp 80-can.network /etc/systemd/network/80-can.network
 sudo cp data.service /etc/systemd/system/data.service
 sudo systemctl enable systemd-networkd
-
-# for USB gadget
-# sudo touch /boot/firmware/ssh
-# setup with /boot/firmware/config.txt dtoverlay=dwc2 and # otg_mode=1
-# setup with /boot/firmware/cmdline.txt modules-load=dwc2,g_ether
